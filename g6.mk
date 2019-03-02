40b21358b102057ea83021b385e1d0c2e7847292
Michael <wolfovelp@gmail.com>
2019-03-02 21:41:01 +0100

    LIGHT: Remove binderised lights, build android.hardware.lights to fix LGE lights dependencies

--- g6.mk
+++ g6.mk
@@ -60,8 +60,8 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.lucye \
-    android.hardware.light@2.0-impl
+	android.hardware.light@2.0-service \
+    	android.hardware.light@2.0-impl
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.lights=h870_sys
