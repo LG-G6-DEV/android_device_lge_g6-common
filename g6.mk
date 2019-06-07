14739b7fa9f6edec109eb67acb28d95b36217e68
Michael Bardoň <wolfovelp@gmail.com>
2019-06-07 22:30:57 +0200

    g6-common: Move sensor_def_common.conf to vendor

--- g6.mk
+++ g6.mk
@@ -56,7 +56,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_lucye.conf:system/etc/sensors/sensor_def_common.conf
+    $(LOCAL_PATH)/configs/sensors/sensor_def_lucye.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_common.conf
 
 # Lights
 PRODUCT_PACKAGES += \
